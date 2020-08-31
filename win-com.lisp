dee50beb7cc627be5e3a94ee64628f2a81c93aba
ChristopherSegale <ccs.segale@gmail.com>
2020-08-30 22:10:54 -0400

    Correcting executable file name.

--- win-com.lisp
+++ win-com.lisp
@@ -1,4 +1,4 @@
 (load "bundle/bundle.lisp")
 (load "snake.asd")
 (asdf:load-system :snake)
-(save-lisp-and-die "snake" :toplevel #'snake:main :executable t :application-type :gui)
+(save-lisp-and-die "snake.exe" :toplevel #'snake:main :executable t :application-type :gui)
